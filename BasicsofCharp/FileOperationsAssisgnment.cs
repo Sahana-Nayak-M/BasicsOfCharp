9b73e7e6405c49b0abc674f2b63e5d1ddfa8e4cf
Sahana-Nayak-M <43909907+Sahana-Nayak-M@users.noreply.github.com>
2022-11-22 20:13:13 +0530

    Add files via upload
    new file

--- BasicsofCharp/FileOperationsAssisgnment.cs
+++ BasicsofCharp/FileOperationsAssisgnment.cs
@@ -14,7 +14,7 @@ namespace BasicsofCharp
         {
             bool flag = true;
             string str=null;
-            string employee = null;
+            string student = null;
             Console.WriteLine("Student Information System");
             while (flag)
             {
@@ -22,25 +22,45 @@ namespace BasicsofCharp
                 Console.WriteLine("Enter options for specified functions");
                 Console.WriteLine("1.Add\n2.Update\n3.Delete\n4.Display\n5.Exit");
                 str=Console.ReadLine();
+                str = str.ToLower();
                 switch (str)
                 {
                     case "1":
-                        Console.WriteLine("Adding student Info,Enter in format (ID,Name,Dept)");
-                        employee = Console.ReadLine();
-                        UpdateFile(employee);
+                    case "add":
+                    case "1.add":
+                        Console.WriteLine("Enter in format (ID,Name,Dept)");
+                        student = Console.ReadLine();
+                        WriteRecord(student);
+                        Console.WriteLine("Added student Info");
                         break;
                     case "2":
-                        Console.WriteLine("Editing student Info");
-
+                    case "update":
+                    case "2.update":
+                        Console.WriteLine("Enter in format (ID,Name,Dept) for record to be updated");
+                        student = Console.ReadLine();
+                        UpdateFile(student);
                         break;
                     case "3":
-                        Console.WriteLine("Deleting student Info");
-
+                    case "delete":
+                    case "3.delete":
+                        Console.WriteLine("Enter student ID to delete\n");
+                        string id = Console.ReadLine();
+                        DeleteInfo(id);
+                        Console.WriteLine("Deleted student Info\n");
+                        ReadFile();
                         break;
                     case "4":
-                        Console.WriteLine("Displaying student Info");
+                    case "display":
+                    case "4.display":
+                        Console.WriteLine("Enter student ID to display");
+                        string id1 = Console.ReadLine();
+
+                        ReadFileID(id1);
+                        
                         break;
                     case "5":
+                    case "exit":
+                    case "5.exit":
                         Console.WriteLine("Exiting..");
                         flag = false;
                         break;
@@ -50,31 +70,73 @@ namespace BasicsofCharp
                 }
             }
         }
-        public void WriteFile()
+        public void WriteRecord(string content)
         {
-            string content = "ID,Name,Dept";
-            File.WriteAllText("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt", content);
+            content = "\n" + content;
+            File.AppendAllText("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt", content);
         }
-        public void UpdateFile(string employee)
+        public void UpdateFile(string content)
         {
-            File.AppendAllText("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt", employee);
+            string[] contents = File.ReadAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt");
+            string id = content.Split(",")[0];
+            string sn = null;
+            for (int i = 0; i < contents.Length; i++)
+            {
+                string iD = contents[i].Split(",")[0];
+
+                //except entered id  all others id's are transferred, (remove that id first from existing) then append new content at last
+                if (id==iD)
+                {
+                    contents[i] = content;              
+                   
+                }
+            }
+            File.WriteAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt", contents);
+            Console.WriteLine("Record is updated");
+            ReadFileID(id);
         }
-        //public void DeleteLine()
-        //{
-        //    if (File.Exists("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt"))
-        //        File.Delete("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt");
-        //    File.D
-        //}
-        public void DeleteFile()
+        
+        
+        
+        public void DeleteInfo(string id)
         {
-            if (File.Exists("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt"))
-                File.Delete("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt");
+            string[] contents = File.ReadAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt");  
+            string[] updated= null;
+            for(int i=0;i<contents.Length;i++)
+            { 
+                string iD =contents[i].Split(",")[0];
+                //except entered id  all others id's are transferred
+                if (id != iD)
+                {
+                    updated[i] = contents[i];
+                      
+                }
+                
+            }
+            File.WriteAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt", updated);
+        
         }
         public void ReadFile()
         {
-            string[] contents = File.ReadAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn//FileIOPractice.txt");
+            string[] contents = File.ReadAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt");
             foreach (string str in contents)
                 Console.WriteLine(str);
+        }
+        public void ReadFileID(string ID)
+        {
+            string[] contents = File.ReadAllLines("C://Users//Sahana_Nayak_M//Documents//FSD Simplilearn and Mosh Angular//FileIOPractice.txt");
+            foreach (string str in contents)
+            {
+                string id = str.Split(",")[0];
+                if (id == ID)
+                {
+                    Console.WriteLine("Please find the info\n");
+                    Console.WriteLine(str);
+                    return;
+                }
+                
+            }
+            Console.WriteLine("Record not found");
         }
     }
 }
